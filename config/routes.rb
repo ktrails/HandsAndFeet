3dc9bb10b462c459d222783d4782b85ea249ccbe
Jeff Kyser <ktrails@comcast.net>
2011-03-22 07:42:19 -0500

    Make posts the default route, and format the posts and comments 'show' method a bit.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ HandsAndFeet::Application.routes.draw do
 
   get "sign_up" => "users#new",:as => "sign_up"
 
-  root :to => "sessions#new"
+  root :to => "posts#index"
 
   resources :posts do
     resources :comments
