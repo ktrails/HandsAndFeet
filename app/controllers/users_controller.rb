022fc290aca153862a41fdaae2f1dbd04085e12e
Jeff Kyser <ktrails@comcast.net>
2011-03-30 18:03:56 -0500

    Used first tip from railscast 259, creating a method and helper to hide instance variables from the views. In other words, create method users to replace access to @users. Conditionally set these so that you don't have to have a bunch of tests. Then, you don't have to go fetch the data unless it is needed by a view.
    Have not implemented the decent-exposure gem yet. Looks like that
    might be hard to remember what in the heck I did later.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,15 @@ class UsersController < ApplicationController
 
   # before_filters: order matters!
   before_filter :logged_in?, :except => [:new]
-  before_filter :find_user, :only => [:destroy, :edit, :show, :update]
   before_filter :format_response, :only => [:edit, :show]
 
   helper_method :sort_column, :sort_direction
 
   def new
-    @user = User.new
   end
 
   def create
-    @user = User.new(params[:user])
-    if @user.save
+    if user.save
       redirect_to root_url, :notice => "Signed up!"
     else
       render "new"
@@ -23,7 +20,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user.destroy if @user
+    user.destroy if user
     respond_to do |format|
       format.html { redirect_to users_url, :notice => "Deleted user!" }
       format.xml  { head :ok }
@@ -35,10 +32,9 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.order(sort_column + " " + sort_direction).search(params[:search]).paginate(:per_page => 8, :page => params[:page])
     respond_to do |format|
       format.html # index.html.erb
-      format.xml { render :xml => @users }
+      format.xml { render :xml => users }
     end
   end
 
@@ -48,13 +44,13 @@ class UsersController < ApplicationController
 
   def update
     respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to(@user,
+      if user.update_attributes(params[:user])
+        format.html { redirect_to(user,
                       :notice => 'Updated user') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @user.errors,
+        format.xml  { render :xml => user.errors,
                       :status => :unprocessable_entity }
 
       end
@@ -62,14 +58,11 @@ class UsersController < ApplicationController
   end
 
   protected
-    def find_user
-      @user = User.find(params[:id])
-    end
 
     def format_response
       respond_to do |format|
         format.html # use corresponding .html.erb file
-        format.xml { render :xml => @user }
+        format.xml { render :xml => user }
       end
     end
 
@@ -82,4 +75,14 @@ class UsersController < ApplicationController
   def sort_direction
     %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
+
+  def users
+    @users ||= User.order(sort_column + " " + sort_direction).search(params[:search]).paginate(:per_page => 8, :page => params[:page])
+  end
+  helper_method :users
+
+  def user
+    @user ||= params[:id] ? User.find(params[:id]) : User.new(params[:user])
+  end
+  helper_method :user
 end
